0ec558176ac1d8a16d82c2fd39888b3f0aaa8547
105991361 <105991361@student.swin.edu.au>
2025-10-19 18:29:30 +1100

    simplified settings.php to match lab 09 approach

--- settings.php
+++ settings.php
@@ -3,17 +3,6 @@
 $host = 'localhost';
 $user = 'root';
 $password = '';
-$dbname = 'sitesquad_db';
+$sql_db = 'sitesquad_db';
 
-$conn = mysqli_connect($host, $user, $password);
-if (!$conn) {
-    die("Connection failed");
-}
-$createDbQuery = "CREATE DATABASE IF NOT EXISTS $dbname";
-if (!mysqli_query($conn, $createDbQuery)) {
-    die("Error creating database: " . mysqli_error($conn));
-}
-if (!mysqli_select_db($conn, $dbname)) {
-    die("Error selecting database: " . mysqli_error($conn));
-}
 ?>
