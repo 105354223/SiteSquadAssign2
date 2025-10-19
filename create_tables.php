2d051cd2bce8a9a0ff0b9f277bcefa16d80f41ca
105991361 <105991361@student.swin.edu.au>
2025-10-19 15:41:45 +1100

    defined initial EOI table structure

--- create_tables.php
+++ create_tables.php
@@ -1,3 +1,11 @@
 <?php
 require_once 'settings.php';
-?>
+
+$createEoiTable = "CREATE TABLE IF NOT EXISTS eoi (
+    eoi_number INT AUTO_INCREMENT PRIMARY KEY,
+    job_reference VARCHAR(10) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    status ENUM('New', 'Current', 'Final') DEFAULT 'New'
+    )";
+?>  
