31a6c2bb80dfe1f735eacf7af3a58cc6f6473a6f
105991361 <105991361@student.swin.edu.au>
2025-10-23 18:38:30 +1100

    Developed secure logout functionality

--- auth.php
+++ auth.php
@@ -12,6 +12,15 @@ function checkAuth() {
     }
 }
 
-
+function logout() {
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+    
+    $_SESSION = array();
+    session_destroy();
+    header('Location: login.php');
+    exit;
+}
 
 ?>
