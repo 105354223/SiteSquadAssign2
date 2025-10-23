66b67e2df1a6630e28dd562eaa342cc976c3e4d8
105991361 <105991361@student.swin.edu.au>
2025-10-23 19:45:03 +1100

    Added basic dashboard with dynamic database info

--- manage.php
+++ manage.php
@@ -2,4 +2,9 @@
 require_once 'settings.php';
 require_once 'auth.php';
 
+echo "<h1>HR Manager Dashboard</h1>";
+echo "<p>Welcome to the EOI Management System</p>";
+echo "<p>Database: $sql_db</p>";
+
+
 ?>
