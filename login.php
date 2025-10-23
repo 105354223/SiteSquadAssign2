3dcfe9bb91df40d6953f2a83411916514eedc068
105991361 <105991361@student.swin.edu.au>
2025-10-23 18:47:29 +1100

    Added form input processing and basic validation

--- login.php
+++ login.php
@@ -3,6 +3,15 @@ require_once 'settings.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Login form submitted
+    
+    $username = trim($_POST['username']);
+    $password = $_POST['password'];
+    
+
+if ($username === 'Admin' && $password === 'Admin') {
+        echo "Login successful!";
+    }
 }
+
 
 ?>
